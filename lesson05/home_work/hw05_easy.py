2be308a66c33fcead05d83213b4b4b34db34ee95
Kein2408 <kein2408@gmail.com>
2019-06-12 23:56:01 +0300

    Complete

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,10 +1,21 @@
+import os
+import shutil
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+def mkdir(dir):
+
+
+n = 9
+#for i in range(n):
+    #os.mkdir(f"directory{i + 1}")
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
+current_list = os.listdir(path=".")
+print(current_list)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
+copied_script = shutil.copy("hw05_easy.py", "hw05_easy_copied.py")
