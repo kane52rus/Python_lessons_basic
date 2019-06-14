3020c3feb6b6ca168b4f28202fe2d31e5fde67a2
oka_tabunkin <kein2408@gmail.com>
2019-06-14 14:40:25 +0300

    Merge branch 'work' of C:\Users\oka_tabunkin\Desktop\python\Python webinar\Home with conflicts.

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -1,64 +1,25 @@
 import os
 import shutil
-import sys
 # Задача-1:
 # Напишите скрипт, создающий директории dir_1 - dir_9 в папке,
 # из которой запущен данный скрипт.
 # И второй скрипт, удаляющий эти папки.
+def mkdir(file_name):
+    os.mkdir(file_name)
 
-def n_mkdir(n):
-    for i in range(n):
-        os.mkdir(f"directory{i + 1}")
-def n_rmdir(n):
-    for i in range(n):
-        os.rmdir(f"directory{i + 1}")
+
+
+
 
 n = 9
+#for i in range(n):
+    #os.mkdir(f"directory{i + 1}")
 
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-def current_dir_list():
-    print(os.listdir(path="."))
+current_list = os.listdir(path=".")
+print(current_list)
 
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
-def copy_file(src_file_name):
-    src_file_name_temp = src_file_name.split(".")
-    dst_file_name = src_file_name_temp[0] + "_copy." + src_file_name_temp[1]
-    shutil.copy(src_file_name, dst_file_name)
-
-
-
-
-
-
-def mkdir(dir_name):
-    if not dir_name:
-        print("Необходимо задать имя директории")
-        return
-    try:
-        os.mkdir(dir_name)
-        print(f"Директория {dir_name} успешно создана!")
-    except FileExistsError:
-        print(f"Директория {dir_name} уже существует!")
-
-def remove_file(dir_name):
-    if not dir_name:
-        print("Необходимо задать имя директории")
-        return
-    try:
-        os.rmdir(dir_name)
-        print(f"Директория {dir_name} успешно удалена!")
-
-    except FileNotFoundError:
-        print(f"Такой директории не существует")
-
-def cd(dir_name):
-    if not dir_name:
-        print("Необходимо задать имя директории")
-        return
-    try:
-        os.chdir(dir_name)
-        print(f"Вы перешли в директорию {os.getcwd()}")
-    except FileNotFoundError:
-        print(f"Директории {dir_name} не существует")
+copied_script = shutil.copy("hw05_easy.py", "hw05_easy_copied.py")
